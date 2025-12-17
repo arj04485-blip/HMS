f2f98626a33145b6d8f74aef25bb0d961080015a
arj04485-blip <arj04485@gmail.com>
2025-12-17 15:54:34 +0530

    Add room rent constants for hostel management

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
+ROOM_RENT = {
+    "Single": 2500,
+    "2 Sharing": 1800,
+    "2 Sharing Attached": 2200,
+    "Triple": 1800
+}
 st.title('hostel management')
 st.subheader('lets make the hostel management eaiser')
